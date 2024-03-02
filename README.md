ab4093661b58db8cffb23f4520e160beea3cf990
fj-blanco <fjavib@gmail.com>
2024-03-02 22:13:25 +0100

    Refactoring createTreeInTabix.py
    Improving input parameters. Added input parameter
    that allow creating the tabix object on the fly,
    without the bash script.
    
    Added documentation.

--- README.md
+++ README.md
@@ -11,3 +11,33 @@ Clone the repository and install the Python requirements into your Python enviro
 ```bash
 pip install -r requirements.txt
 ```
+
+On Ubuntu/Debian-based Linux Distributions:
+
+You need to install `tabix` from the official package repositories by running:
+
+### On Ubuntu/Debian-based Linux Distributions
+
+```bash
+sudo apt-get update
+sudo apt-get install -y tabix
+```
+
+## Creating the tree in Tabix format
+
+Get the [YFull tree](https://github.com/YFullTeam/YTree), for example (you may want to check for the latest version):
+
+```bash
+mkdir -p yfull_tree
+cd yfull_tree
+wget -O ytree.zip https://github.com/YFullTeam/YTree/blob/master/ytree/tree_11.05.0.zip?raw=true
+unzip ytree.zip
+rm ytree.zip
+cd ..
+```
+
+Now create the tree in Tabix format:
+
+```bash
+python createTreeInTabix.py yfull_tree/tree_11.05.0.json --create_tabix_files
+```
--- createTreeInTabix.py
+++ createTreeInTabix.py
@@ -5,27 +5,16 @@ Created on Fri Dec 20 13:22:52 2019
 @author: hunte
 """
 
-import sys
+import os
 import json
+import argparse
+import subprocess
 
-if len(sys.argv) > 8:
-    treeFile = sys.argv[1]
-    hg19positionMarkersTSV = sys.argv[2]
-    hg38positionMarkersTSV = sys.argv[3]
-    cladeSNPFilePath = sys.argv[4]
-    SNPcladeFilePath = sys.argv[5]
-    hg19positionMarkersFilePath = sys.argv[6]
-    hg38positionMarkersFilePath = sys.argv[7]
-    productsFilePath = sys.argv[8]
-    toIgnoreFilePath = sys.argv[9]
-else:
-    treeFile = "C:\clade-finder-files\yfull.json"
-    cladeSNPFilePath = "C:\clade-finder-files\cladeSNPs"
-    SNPcladeFilePath = "C:\clade-finder-files\SNPclades"
-    
 hierarchy = {}
 childMap = {}
 snps = {}
+
+TABIX_OUTPUT_DIR = "./tabix_output"
 
 def parseTreeJSON(fil):
     thefile = open(fil)
@@ -33,10 +22,6 @@ def parseTreeJSON(fil):
     thefile.close()
     recurseTreeJson(root)
     return (root["id"])
-
-#remove parens
-#replace plus with PLUS
-#replace minus with MINUS
 
 def replaceAsNecessary(snp):
     return snp.replace("(","_L_PAREN_").replace(")","_R_PAREN_").replace("+","_PLUS_").replace("-","_MINUS_").replace(" ","").replace(".","_DOT_")
@@ -49,8 +34,6 @@ def getToIgnore(file):
             if snp != "":
                 ignore.append(snp)
     return ignore
-        
-toIgnore = getToIgnore(toIgnoreFilePath) #["PF6234"]
 
 def parseSNPsString(snpsString):
     thesnps = set([])
@@ -71,56 +54,9 @@ def recurseTreeJson(node):
                 childMap[node["id"]].append(child["id"])
                 hierarchy[child["id"]] = node["id"]
                 snps[child["id"]] = parseSNPsString(child["snps"])
-                recurseTreeJson(child)
-
-def createTextFile(cladeSNPFilePath, SNPcladeFilePath, uniqSnpToProducts):
-    with open(cladeSNPFilePath, "w") as w:
-        for clade in snps:
-            for snp in snps[clade]:
-                w.write("\t".join([clade, "1", "1", snp, "."]) + "\n")
-            if clade in hierarchy:
-                w.write("\t".join([clade, "2", "2", hierarchy[clade], "."]) + "\n")
-            if clade in childMap:
-                for child in childMap[clade]:
-                    w.write("\t".join([clade, "3", "3", child, "."]) + "\n")
-    w.close()
-    with open(SNPcladeFilePath,  "w") as w:
-        for clade in snps:
-            for snp in snps[clade]:
-                w.write("\t".join([snp, "1", "1", clade, "."]) + "\n")
-                if "/" in snp:
-                    for same_name_snp in snp.split("/"):
-                        if same_name_snp not in toIgnore:
-                            w.write("\t".join([same_name_snp, "2", "2", snp, "."]) + "\n")
-        for uniqSNP in uniqSnpToProducts:
-            w.write("\t".join([uniqSNP, "3", "3", uniqSnpToProducts[uniqSNP], "."]) + "\n")
-    w.close()
-    with open(hg19positionMarkersFilePath, "w") as w:
-        with open(hg19positionMarkersTSV, "r") as r:
-            for line in r.readlines():
-                splt = line.replace("\n","").split("\t")
-                if len(splt) == 3 and splt[0] != "":
-                    marker_safe = replaceAsNecessary(splt[1])             
-                    w.write("\t".join([splt[0], "1", "1", marker_safe, splt[2]]) + "\n")
-                else:
-                    print("ignored: " + ",".join(splt))
-        r.close()
-    w.close()
-    with open(hg38positionMarkersFilePath, "w") as w:
-        with open(hg38positionMarkersTSV, "r") as r:
-            for line in r.readlines():
-                splt = line.replace("\n","").split("\t")
-                if len(splt) == 3 and splt[0] != "":
-                    marker_safe = replaceAsNecessary(splt[1])             
-                    w.write("\t".join([splt[0], "1", "1", marker_safe, splt[2]]) + "\n")
-                else:
-                    print("ignored: " + ",".join(splt))
-        r.close()
-    w.close()
-        
+                recurseTreeJson(child)        
 
 def getMappingOfSamenameSNPtoUniq():
-    
     uniqsnps = set([])
     for clade in snps:
         for snp in snps[clade]:
@@ -152,7 +88,108 @@ def getUniqSNPtoProducts(productsFilePath):
             uniqSNPtoProducts[snp] = snpToProducts[snp]
     return uniqSNPtoProducts
 
-           
-parseTreeJSON(treeFile)
-uniqSnpToProducts = getUniqSNPtoProducts(productsFilePath)
-createTextFile(cladeSNPFilePath, SNPcladeFilePath, uniqSnpToProducts)
+def processPositionMarkers(inputTSVPath, outputFilePath):
+    with open(outputFilePath, "w") as w:
+        with open(inputTSVPath, "r") as r:
+            for line in r.readlines():
+                splt = line.strip().split("\t")
+                if len(splt) == 3 and splt[0] != "":
+                    marker_safe = replaceAsNecessary(splt[1])
+                    w.write("\t".join([splt[0], "1", "1", marker_safe, splt[2]]) + "\n")
+                else:
+                    print("ignored: " + ",".join(splt))
+
+def create_tabix_file(file_path):
+    # Determine the base name for the file without the .tsv extension
+    base_name = os.path.basename(file_path).replace('.tsv', '')
+    output_path = os.path.join(TABIX_OUTPUT_DIR, base_name)
+
+    # Sort, compress, and create the bgz file
+    sort_command = f"sort {file_path} -k1,1 -k2n | bgzip > {output_path}.bgz"
+    subprocess.run(sort_command, shell=True, check=True)
+
+    # Index the compressed file
+    tabix_command = f"tabix -s 1 -b 2 -e 3 {output_path}.bgz"
+    subprocess.run(tabix_command, shell=True, check=True)
+
+def createTextFile(cladeSNPFilePath, SNPcladeFilePath, uniqSnpToProducts, toIgnore=[], 
+                   hg19positionMarkersTSV=None, hg38positionMarkersTSV=None, 
+                   hg19positionMarkersFilePath=None, hg38positionMarkersFilePath=None):
+    # Handle Clade to SNP and SNP to Clade mappings
+    # We create the cladeSNP file in the TABIX_OUTPUT_DIR folder if it doesn't exist
+
+    with open(cladeSNPFilePath, "w") as w:
+        for clade in snps:
+            for snp in snps[clade]:
+                w.write("\t".join([clade, "1", "1", snp, "."]) + "\n")
+            if clade in hierarchy:
+                w.write("\t".join([clade, "2", "2", hierarchy[clade], "."]) + "\n")
+            if clade in childMap:
+                for child in childMap[clade]:
+                    w.write("\t".join([clade, "3", "3", child, "."]) + "\n")
+
+    # We create the SNPclade file in the TABIX_OUTPUT_DIR folder if it doesn't exist
+    with open(SNPcladeFilePath, "w") as w:
+        for clade in snps:
+            for snp in snps[clade]:
+                w.write("\t".join([snp, "1", "1", clade, "."]) + "\n")
+                if "/" in snp:
+                    for same_name_snp in snp.split("/"):
+                        if same_name_snp not in toIgnore:
+                            w.write("\t".join([same_name_snp, "2", "2", snp, "."]) + "\n")
+        for uniqSNP in uniqSnpToProducts:
+            w.write("\t".join([uniqSNP, "3", "3", uniqSnpToProducts[uniqSNP], "."]) + "\n")
+
+    # Handle hg19 position markers if file paths are provided
+    if hg19positionMarkersTSV and hg19positionMarkersFilePath:
+        processPositionMarkers(hg19positionMarkersTSV, hg19positionMarkersFilePath)
+
+    # Handle hg38 position markers if file paths are provided
+    if hg38positionMarkersTSV and hg38positionMarkersFilePath:
+        processPositionMarkers(hg38positionMarkersTSV, hg38positionMarkersFilePath)
+
+def main(args):
+    toIgnore = getToIgnore(args.toIgnoreFilePath) if args.toIgnoreFilePath else []
+    treeFile = args.treeFile
+    parseTreeJSON(treeFile)
+
+    # Handle optional arguments
+    if args.productsFilePath:
+        uniqSnpToProducts = getUniqSNPtoProducts(args.productsFilePath)
+    else:
+        uniqSnpToProducts = {}
+    if not args.cladeSNPFilePath:
+        cladeSNPFilePath = f"{TABIX_OUTPUT_DIR}/cladeSNP.tsv"
+    if not args.SNPcladeFilePath:
+        SNPcladeFilePath = f"{TABIX_OUTPUT_DIR}/SNPclade.tsv"
+
+    # Create the cladeSNP and SNPclade files
+    createTextFile(cladeSNPFilePath, SNPcladeFilePath, uniqSnpToProducts, toIgnore)
+    # Create the tabix files if the flag is set
+    if args.create_tabix_files:
+        print("Creating tabix files...")
+        create_tabix_file(cladeSNPFilePath)
+        create_tabix_file(SNPcladeFilePath)
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Process a phylogenetic tree and generate SNP and clade mappings.')
+    parser.add_argument('treeFile', type=str, help='Path to the YFull tree JSON file.')
+    parser.add_argument('--cladeSNPFilePath', type=str, default='', help='Path to the output file for clade to SNP mappings. Optional')
+    parser.add_argument('--SNPcladeFilePath', type=str, default='', help='Path to the output file for SNP to clade mappings. Optional')
+    parser.add_argument('--hg19positionMarkersTSV', type=str, default='', help='Path to the TSV file with hg19 position markers. Optional.')
+    parser.add_argument('--hg38positionMarkersTSV', type=str, default='', help='Path to the TSV file with hg38 position markers. Optional.')
+    parser.add_argument('--hg19positionMarkersFilePath', type=str, default='', help='Output path for the processed hg19 position markers. Optional.')
+    parser.add_argument('--hg38positionMarkersFilePath', type=str, default='', help='Output path for the processed hg38 position markers. Optional.')
+    parser.add_argument('--productsFilePath', type=str, default='', help='Path to the products file mapping unique SNPs to YSEQ products. Optional.')
+    parser.add_argument('--toIgnoreFilePath', type=str, default='', help='Path to the file listing SNPs to ignore. Optional.')
+    parser.add_argument('--create_tabix_files', action='store_true', help='Create tabix files for cladeSNP and SNPclade files.')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+    # if cladeSNPFilePath or SNPcladeFilePath are not provided, we create them in the TABIX_OUTPUT_DIR folder
+    if not args.cladeSNPFilePath or not args.SNPcladeFilePath:
+        # Ensure the TABIX_OUTPUT_DIR exists
+        if not os.path.exists(TABIX_OUTPUT_DIR):
+            os.makedirs(TABIX_OUTPUT_DIR)
+    main(args)
